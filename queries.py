32d5b329223c758aa6023e9fbf40c72d77fc00b8
hamub <hbutau35@gmail.com>
2019-04-12 01:12:06 +0200

    qauter

--- queries.py
+++ queries.py
@@ -384,9 +384,12 @@ if __name__ == '__main__':
     print(Entry.objects.filter(pub_date__week=52))
 
 
-   # week_day
-   print(Entry.objects.filter(pub_date__week_day=2))
-   print(Entry.objects.filter(pub_date__week_day__gte=2))
+    # week_day
+    print(Entry.objects.filter(pub_date__week_day=2))
+    print(Entry.objects.filter(pub_date__week_day__gte=2))
+
+    # quarter
+    print(Entry.objects.filter(pub_date__quarter=2))
 
 
 
